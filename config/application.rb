b92521b380304f805872fc091264b9327f15971f
iaydiner <lysydnr@gmail.com>
2016-09-06 10:20:28 -0400

    change to rails image_tag v20

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Europe
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
-    config.assets.paths << Rails.root.join("app", "assets", "slider_assets", "video")
+   # config.assets.paths << Rails.root.join("app", "assets","video")
+    config.assets.paths << Rails.root.join("app", "assets","slider_assets","video")
   end
 end
